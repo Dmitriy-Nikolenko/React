eb3e91221535b2820e51003e50639c9eb27b8e74
Dmitriy-Nikolenko <59972104+Dmitriy-Nikolenko@users.noreply.github.com>
2021-07-29 20:59:59 +0300

    Домашняя работа к первому уроку

---
+++ src/AppClass.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export class AppClass extends Component {
+    render() {
+        return(
+            <div className="App">
+            <header className="App-header">AppClass</header>
+          </div> 
+        )
+    }
+ }
