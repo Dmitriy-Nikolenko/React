b16efcbe78d250b11d361eef1e111ae67d94fc79
Dmitriy-Nikolenko <dimanikolenko1986@gmail.com>
2021-08-09 00:37:51 +0300

    Выполнил ДЗ к 4 уроку

---
+++ src/app-hooks.js
@@ -0,0 +1,4 @@
+export const AppHooks = () => {
+    return null;
+
+};
---
+++ src/components/message-list/message-list.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {Button, makeStyles, Input} from '@material-ui/core';
+import { useState, useEffect } from "react";
+import { Send } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+      margin: theme.spacing(1),
+    },
+  }));
+
+export const MessageList = () => {
+    const [messageList, setMessageList] = useState([])
+
+    const [value, setValue] = useState ("")
+    
+
+      
+    const handleSendMessage = () => {
+        setMessageList((state) => [...state, {value, author: "User"}])
+        setValue("");
+       };
+    const classes = useStyles();
+    
+    useEffect(() => {  
+        const lastMessageList = messageList[messageList.length - 1]
+
+        if (lastMessageList?.author === "User") {
+            setTimeout(() => {
+                setMessageList((state) => [
+                    ...state, {
+                        value: "Ваше сообщение добавлено", author: "Bot"
+                    }
+                ])
+            }, 500)
+        }
+
+    }, [messageList])
+
+    return (
+        <div >
+            <ul>
+                {messageList.map((message, id) => <li key={id}>{message.value} = {message.author}</li>)}
+            </ul>
+    <Input 
+           placeholder="Введите сообщение" 
+           inputProps={{ 'aria-label': 'description' }} 
+           autoFocus={true} 
+           value={value} 
+           onChange = {(e) => setValue(e.target.value)}
+           >
+
+    </Input>
+                       
+    <Button
+            onClick={handleSendMessage}
+            variant="contained"
+            color="primary"
+            className={classes.button}            
+      >
+        Send
+        <Send></Send>
+      </Button>
+        
+        </div>
+    )
+        
+    
+}
