12f20e79aae578e5ac363d5f3d84a4d25619ba71
Dmitriy-Nikolenko <dimanikolenko1986@gmail.com>
2021-09-01 22:51:00 +0300

    Выполнил ДЗ после 10 урока

--- src/store/messages/reducer.js
+++ src/store/messages/reducer.js
@@ -1,4 +1,11 @@
-import {GET_MESSAGES_ERROR, GET_MESSAGES_START, GET_MESSAGES_SUCCESS,  SEND_MESSAGE_ERROR,  SEND_MESSAGE_START,  SEND_MESSAGE_SUCCES } from "./types"
+import {
+  GET_MESSAGES_ERROR,
+  GET_MESSAGES_START,
+  GET_MESSAGES_SUCCESS,
+  SEND_MESSAGE_ERROR,
+  SEND_MESSAGE_START,
+  SEND_MESSAGE_SUCCES,
+} from "./types"
 
 const initialState = {
   messages: {},
@@ -11,35 +18,34 @@ export const messagesReducer = (state = initialState, action) => {
     case SEND_MESSAGE_SUCCES:
     case SEND_MESSAGE_ERROR:
       return {
-        ...state,
         messages: {
           ...state.messages,
           [action.payload.roomId]: [
-            ...state.messages[action.payload.roomId],
+            ...(state.messages[action.payload.roomId] || []),
             { ...action.payload.message, id: new Date() },
           ],
           messagesPending: false,
         },
       }
-      case GET_MESSAGES_START:
-      case SEND_MESSAGE_START:
-        return {
-          ...state,
-          messagesPending: true,
-        } 
+    case GET_MESSAGES_START:
+    case SEND_MESSAGE_START:
+      return {
+        ...state,
+        messagesPending: true,
+      }
 
-      case GET_MESSAGES_SUCCESS:
-        return {
-          ...state,
-          messages: action.payload,
-          messagesPending: false,
-        }  
-      case GET_MESSAGES_ERROR:
-         return {
-          ...state,
-          messagesError: action.payload,
-          messagesPending: false,
-        } 
+    case GET_MESSAGES_SUCCESS:
+      return {
+        ...state,
+        messages: action.payload,
+        messagesPending: false,
+      }
+    case GET_MESSAGES_ERROR:
+      return {
+        ...state,
+        messagesError: action.payload,
+        messagesPending: false,
+      }
     default:
       return state
   }
---
+++ src/utils/render-with-redux.js
@@ -0,0 +1,20 @@
+import { render } from "@testing-library/react"
+import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
+import { createStore } from "redux"
+import { reducer } from "../components"
+
+export const renderWithRedux = (component, { initialState}) => {
+const store = createStore(reducer, initialState)
+
+return {
+    store,
+    ...render(
+        <Provider store={store}>
+          <BrowserRouter>
+            {component}
+          </BrowserRouter>
+        </Provider>,
+      )
+}
+}
